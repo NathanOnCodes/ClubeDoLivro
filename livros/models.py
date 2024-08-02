bdb4a50e1fb5646b9cc742c501a0e440ac178400
NathanOnCodes <contatonathancf@gmail.com>
2024-08-01 23:29:13 -0300

    feat: model livros

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Livros(models.Model):
+    id_livro = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    titulo = models.charField(max_length=255)
+    autor = models.charField(max_length=255)
+    ano_lancamento = models.IntegerField()
+    estado_livro = models.charField(max_length=50)
+    paginas = models.IntegerField()
+    editora = models.charField(max_length=255)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    
