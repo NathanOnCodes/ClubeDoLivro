4b8c8e0f6788942929bf182339d4ade15bd6a917
NathanOnCodes <contatonathancf@gmail.com>
2025-02-13 16:32:17 -0300

    feat: config jwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants/constants';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '300000s' },
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
