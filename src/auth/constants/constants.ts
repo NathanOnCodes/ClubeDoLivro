72f4d8dfc82480c7cf3f57a1a6a9110720121287
NathanOnCodes <contatonathancf@gmail.com>
2025-02-13 16:20:48 -0300

    chore: configure constants

--- src/auth/constants/constants.ts
+++ src/auth/constants/constants.ts
@@ -1,7 +1,8 @@
 import { SetMetadata } from "@nestjs/common";
 
 export const jwtConstants = {
-    secret: ''
+    secret: "TEACH ME TO DO YOUR WILL, FOR YOU ARE MY GOD; MAY YOUR GOOD SPIRIT LEAD ME ON LEVEL GROUND.\
+    FOR YOUR NAME'S SAKE, LORD, PRESERVE MY LIFE; IN YOUR RIGHTEOUSNESS, BRING ME OUT OF TROUBLE"
 };
 
 export const IS_PUBLIC_KEY = 'isPublic';
