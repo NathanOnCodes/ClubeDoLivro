0ea0ea71c8b62ecc1ea1dd674c98270c3b7b8621
NathanOnCodes <contatonathancf@gmail.com>
2025-02-19 00:51:42 -0300

    feat: new redis service and config shared module

--- src/shared/redis/redis.service.ts
+++ src/shared/redis/redis.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
+    ) {}
+
+    async get(key: string): Promise<any> {
+        return this.cacheManager.get(key);
+    }
+
+    async set(key: string, value: any, ttl?: number): Promise<void> {
+        await this.cacheManager.set(key, value, ttl);
+    }
+
+    async del(key: string): Promise<void> {
+        await this.cacheManager.del(key);
+    }
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
-import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 import { RedisService } from './redis/redis.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 
 @Module({
-  providers: [RabbitmqService, RedisService]
+  imports: [
+    CacheModule.register({
+        store: redisStore,
+        host: 'localhost',
+        port: 6379,
+        password: process.env.REDIS_PASSWORD, // Use as variáveis do seu .env
+        ttl: 600, // TTL padrão em segundos (opcional)
+      }),
+  ],
+  providers: [RabbitmqService, RedisService],
+  exports: [RedisService]
 })
 export class SharedModule {}
