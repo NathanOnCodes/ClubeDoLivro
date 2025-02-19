324626cc273d677e804ab04bd15aef88f2c891a5
NathanOnCodes <contatonathancf@gmail.com>
2025-02-19 00:47:39 -0300

    feat: add group entity

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,1 +1,21 @@
-export class Group {}
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Group {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @ManyToOne(() => User, (user) => user.ownedGroups)
+    owner: User;
+
+    @ManyToMany(() => User, (user) => user.participatingGroups)
+    @JoinTable() 
+    participants: User[];
+
+    @Column({ default: 'pending' })
+    status: 'pending' | 'drawn';
+}
