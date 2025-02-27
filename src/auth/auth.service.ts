05575334d660eb59831665e49deb2f761076af90
NathanOnCodes <contatonathancf@gmail.com>
2025-02-27 00:59:14 -0300

    feat: auth methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,70 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './dto/auth.dto';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { HashService } from './hash.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        private readonly hashService: HashService,
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+        private readonly jwtService: JwtService
     ) {}
 
 
-    async signIn(email: string): Promise<any>{
-        //const user = await this.usersService.findOne(email);
-        // if (!user){
-        //     throw new UnauthorizedException();
-        // }
-        // const payload = { userEmail: user.email, userName: user.name };
-        // const token = {
-        //     access_token: await this.jwtService.signAsync(payload),
-        // };
-        // return token;
+    async login(loginDto: LoginDto){
+        const user = await this.userRepository.findOne({where: {email: loginDto.email}});
+        if(!user) throw new UnauthorizedException();
+
+        const passwordValidate = await this.hashService.compare(loginDto.password, user.password);
+        if(!passwordValidate) throw new UnauthorizedException();
+
+        return this.createTokens(user);
+    }
+
+    private async createTokens(user: User){
+        const accessTokenPromise = this.signJwtAsync<Partial<User>>(
+            user.id,
+            this.jwtConfiguration.jwtTtl,
+            { email: user.email },
+          );
+      
+          const refreshTokenPromise = this.signJwtAsync(
+            user.id,
+            this.jwtConfiguration.jwtRefreshTtl,
+          );
+      
+          const [accessToken, refreshToken] = await Promise.all([
+            accessTokenPromise,
+            refreshTokenPromise,
+          ]);
+      
+          return {
+            accessToken,
+            refreshToken,
+          };
+    }
+
+    private async signJwtAsync<T>(sub: number, expiresIn: number, payload?: T){
+        return  this.jwtService.signAsync(
+            {
+                sub,
+                ...payload
+            },
+            {
+                audience: this.jwtConfiguration.audience,
+                issuer: this.jwtConfiguration.issuer,
+                secret: this.jwtConfiguration.secret,
+                expiresIn,
+            }
+        );
     }
 }
